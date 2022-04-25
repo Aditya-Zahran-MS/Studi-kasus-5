c25ee06620f773e4030d5ee7012e57cc898e4739
Ghaitsa Zahira Rahma <101691351+ghaitsazahirar@users.noreply.github.com>
2022-04-25 23:05:53 +0700

    Update inputoutput.cpp

--- inputoutput.cpp
+++ inputoutput.cpp
@@ -1,43 +1,39 @@
-//#include "../base.h"
-#include<iostream>
-using namespace std;
-class Sks{
-public:
-  void input();
-  void output();
-private:
+#include "../base.h"
+
+  void sks::input(){
   int total_sks[20], nim, matkul;
   string matskul[20], nama;
- // int diskon, jumlah_bayar;
-  int jumlah_bayar = 300000;
-  int diskon = 75000;
-};
-  void Sks::input(){
-  
   cout<<"========="<<endl;
-  cout<<"SKS"<<endl;
+  cout<<"|| SKS ||"<<endl;
   cout<<"========="<<endl;
   cout<<"Daftar Matkul"<<endl;
   cout<<"1. Alpro"<<endl;
+  cout<<"2. Pweb"<<endl;
+  cout<<"3. Matdisk"<<endl;
+  cout<<"4. Alin"<<endl;
+  cout<<"5. Arsikomp"<<endl;
   cout<<"Masukkan Nim  : ";cin>>nim;
   cout<<"Masukkan Nama  : ";cin>>nama;
   cout<<"Masukkan jumlah matkul : ";cin>>matkul;
-  for(int i=0;i<=matkul;i++)
+  cout<<"=========================="<<endl;
+  for(int i=0;i<matkul;i++)
     {
-      cout<<"Masukkan jumlah SKS  : ";cin>>total_sks[i];
-      cout<<"Matkul : ";cin>>matskul[i];
+      cout<<"Nama Matkul : ";cin>>matskul[i];
+      cout<<"Masukkan jumlah SKS per matkul : ";cin>>total_sks[i];
     }
-  
+  cout<<"=========================="<<endl;
     }
-void Sks::output()
+void sks::output()
 {
+  int jumlah_bayar = 300000;
+  int diskon = 75000;
   cout<<"Masukan total bayar : "<<jumlah_bayar<<endl;
   cout<<"Diskon : "<<diskon;
 }
 
 int main()
 {
-  Sks esks;
+  sks esks;
   esks.input();
   esks.output();
 }
